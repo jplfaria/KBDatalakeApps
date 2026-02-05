fd96432b7872e3620bdf82eb0c25f6b629337c29
Filipe Liu <fliu@anl.gov>
2026-02-05 00:11:29 -0600

    test parses

--- lib/KBDatalakeApps/annotation/annotation.py
+++ lib/KBDatalakeApps/annotation/annotation.py
@@ -8,7 +8,10 @@ def parse_kofam(data: str):
             _parts = line.strip().split()
             if len(_parts) == 2:
                 feature_id, ko = _parts
-                annotation[feature_id] = ko
+                if feature_id not in annotation:
+                    annotation[feature_id] = set()
+                annotation[feature_id].add(ko)
+
     return annotation
 
 
@@ -17,8 +20,16 @@ def parse_bakta(data: str):
 
 
 def parse_psortb(data: str):
-    
-    pass
+    annotation = {}
+    lines = data.split('\n')
+    h = lines[0].strip()
+    # skip header
+    for line in lines[1:]:
+        r = line.strip().split('\t')
+        d = {h[i]: r[i].strip() for i in range(len(h))}
+        annotation[d['SeqID']] = d
+
+    return annotation
 
 
 def test_annotation(client_kofam, client_bakta, client_psortb, client_rast):
@@ -37,6 +48,7 @@ def test_annotation(client_kofam, client_bakta, client_psortb, client_rast):
         print(f"Execution time: {end_time - start_time} seconds")
         print(f'received results of type {type(result)} and size {len(result)}')
         print(result)
+        print('parse', parse_psortb(result))
     except Exception as ex:
         print(f'nope {ex}')
 
@@ -59,16 +71,25 @@ def test_annotation(client_kofam, client_bakta, client_psortb, client_rast):
         print(f"Execution time: {end_time - start_time} seconds")
         print(f'received results of type {type(result)} and size {len(result)}')
         print(result)
+        print('parse', parse_psortb(result))
     except Exception as ex:
         print(f'nope {ex}')
 
     try:
         print(f"test RAST_SDK annotation")
         start_time = time.perf_counter()
-        result = client_rast.annotate_proteins({'proteins': list(proteins.values())})
+        l_sequences = []
+        l_feature_id = []
+        for i, s in proteins.items():
+            l_sequences.append(s)
+            l_feature_id.append(i)
+        result = client_rast.annotate_proteins({'proteins': l_sequences})
         end_time = time.perf_counter()
         print(f"Execution time: {end_time - start_time} seconds")
         print(f'received results of type {type(result)} and size {len(result)}')
         print(result)
+        annotation = {}
+        for i in range(len(l_feature_id)):
+            annotation[l_feature_id[i]] = result[i]
     except Exception as ex:
         print(f'nope {ex}')
